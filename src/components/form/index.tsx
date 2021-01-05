0e0cb9d38a74b91f82ed5a440571ce5816b55fe6
liubiao <liubiao@findpiano.com>
2021-01-05 13:24:53 +0800

    create templete files

---
+++ src/components/form/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css'
+import { Button } from "antd"
+const Index = () => {
+  return (
+    <div>
+      <Button>button</Button>
+    </div>
+  )
+}
+export default Index;
